a4cdb50c6cc96e5353de0b69cc356d474deffd43
timothy battafarano <tim@deepsprings.edu>
2024-02-28 14:25:22 -0600

    deleted wishlist.dart contents

--- lib/src/features/wishlist/presentation/home_app_bar.dart
+++ lib/src/features/wishlist/presentation/home_app_bar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:secret_santa/src/features/participants/data/fake_participants_repository.dart';
 
 class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
+  //TODO: either remove 'implements PreferredSizeWidget' or use it
   const HomeAppBar({super.key});
   
   @override
@@ -14,12 +15,17 @@ class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
         return Container(
+          color: Colors.green,
+          padding: EdgeInsets.all(8.0),
           width: 150,
           height: 150,
           child: ListTile(
             key: Key('$index'),
-            title: Text(
-            participantsList[index].name)),
+            title: ElevatedButton(
+              onPressed: null,
+              child: Text(
+              participantsList[index].name),
+            )),
         );
         }
       );
