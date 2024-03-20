b376fe4e554f015398122aee53241f62cc292e85
timothy battafarano <tim@deepsprings.edu>
2024-03-20 18:28:50 -0500

    changed wishlist_list.dart to wishlist_view.dart and refactord wishList variable in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:secret_santa/src/features/participants/data/fake_participants_repository.dart';
 import 'package:secret_santa/src/features/wishlist/presentation/home_app_bar.dart';
+import 'package:secret_santa/src/features/wishlist/presentation/wish.dart';
 import 'package:secret_santa/src/features/wishlist/presentation/wishlist_text_bar.dart.dart';
-import 'package:secret_santa/src/features/wishlist/presentation/wishlist_list.dart';
-
+import 'package:secret_santa/src/features/wishlist/presentation/wishlist_view.dart';
 
 void main() {
   runApp(ProviderScope(child: MyApp()));
@@ -16,57 +16,24 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
-  // List<String> wishList = FakeParticipantsRepository.instance.getWishesList('1');
-
-  List<String> _unmodifiableWishList =
-      FakeParticipantsRepository.instance.getWishesList('2');
-  // List<String>  wishList = List.from(unmodifiableWishList); 
-  List<String> get wishList => _unmodifiableWishList.toList();
-  // void makeWishlist (wishList) {
-  //   wishList; 
-  // }
-  
-  // makeWishlist () {
-  //   wishList = unmodifiableWishList.toList();
-  //   }
-  // List<String> addWishes(unmodifiableWishList) {
-    // wishList = unmodifiableWishList.toList();
-  //   return wishList;
-
-
+  List<Wish> wishList = ref.watch(wishesProvider);
 
   @override
   Widget build(BuildContext context) {
     final ColorScheme colorScheme = Theme.of(context).colorScheme;
     // addWishes(unmodifiableWishList);
     return Scaffold(
-      
-      body: 
-      Center(
-        child: 
-        
-        Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+          Expanded(flex: 1, child: HomeAppBar()),
           Expanded(
-            flex: 1,
-            child: HomeAppBar()),
-          Expanded(
-          flex: 8,
-          child: wishList.isEmpty
-              ? Center(child: Text('Add Items to Your Wishlist!'))
-              : WishlistList()),
+              flex: 8,
+              child: wishList.isEmpty
+                  ? Center(child: Text('Add Items to Your Wishlist!'))
+                  : WishlistView()),
           WishlistTextBar(),
-        ]  
-      )
-      )
-    );
-    
+        ])));
   }
 }
-
-
-
-
-
-
